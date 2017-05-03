2ea7161d13d4a065feec46368bdffad24262f9e0
Charles Clemens <cclemensjr@gmail.com>
2017-05-03 03:30:18 -0700

    Add delete feature to the app

---
+++ spec/features/deleting_posts_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper.rb'
+
+feature 'Deleting posts' do
+  background do
+    post = create(:post)
+
+    visit '/'
+    find(:xpath, "//a[contains(@href,'posts/1')]").click
+    click_link 'Edit'
+  end
+
+  scenario 'Can delete a post' do
+    click_link 'Delete'
+
+    expect(page).to have_content("Problem solved! Post deleted.")
+    expect(page).to_not have_content('Abs for days')
+  end
+end
