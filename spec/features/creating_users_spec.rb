55f21aeeebffbc5237c41b1b4349ed81b3da7ff8
Charles Clemens <cclemensjr@gmail.com>
2017-05-15 12:53:49 -0700

    Update user feature

--- spec/features/creating_users_spec.rb
+++ spec/features/creating_users_spec.rb
@@ -15,4 +15,33 @@ feature 'Creating a new user' do
     click_button 'Sign up'
     expect(page).to have_content('Welcome! You have signed up successfully.')
   end
+
+  scenario 'requires a user name to successfully create an account' do
+    fill_in 'Email',                  with: 'sxyrailsdev@myspace.com'
+    fill_in 'Password',               with: 'supersecret', match: :first
+    fill_in 'Password confirmation',  with: 'supersecret'
+
+    click_button 'Sign up'
+    expect(page).to have_content('You need a user name to create an account.')
+  end
+
+  scenario 'requires a user name to be more than 4 characters' do
+    fill_in 'User name',              with: 'h'
+    fill_in 'Email',                  with: 'sxyrailsdev@myspace.com'
+    fill_in 'Password',               with: 'supersecret', match: :first
+    fill_in 'Password confirmation',  with: 'supersecret'
+
+    click_button 'Sign up'
+    expect(page).to have_content('minimum is 4 characters')
+  end
+
+  scenario 'requires a user name to be less than 12 characters' do
+    fill_in 'User name',              with: 'h' * 13
+    fill_in 'Email',                  with: 'sxyrailsdev@myspace.com'
+    fill_in 'Password',               with: 'supersecret', match: :first
+    fill_in 'Password confirmation',  with: 'supersecret'
+
+    click_button 'Sign up'
+    expect(page).to have_content('maximum is 12 characters')
+  end
 end
